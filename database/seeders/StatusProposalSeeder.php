f59c2b4e7b08c7960eeda54c7e412862704f4585
burhanudinm81 <burhanudinm81@gmail.com>
2025-07-01 11:42:26 +0700

    Menambahkan StatusProposalSeeder

--- database/seeders/StatusProposalSeeder.php
+++ database/seeders/StatusProposalSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\StatusProposal;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,16 @@ class StatusProposalSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $status1 = new StatusProposal();
+        $status1->status = "Diterima tanpa revisi";
+        $status1->save();
+
+        $status2 = new StatusProposal();
+        $status2->status = "Diterima dengan revisi";
+        $status2->save();
+
+        $status3 = new StatusProposal();
+        $status3->status = "Ditolak";
+        $status3->save();
     }
 }
