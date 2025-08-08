f5beae440557614d06ba37c1178657b3526ab160
burhanudinm81 <burhanudinm81@gmail.com>
2025-08-08 13:28:42 +0700

    Perubahan Minor 1. Membuat Footer di Halaman Panitia (Sebelumnya file panitia.footer masih kosong) 2. Memperbaiki bug: bar untuk menutup dan membuka sidebar tidak berfungsi 3. Menghapus beberapa Komentar di file home, navbar, sidebar, dan footer

--- resources/views/dosen/footer.blade.php
+++ resources/views/dosen/footer.blade.php
@@ -5,5 +5,5 @@
         Sistem Informasi dan Arsip Naskah Tugas Akhir Telekomunikasi
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2025 <a href="#">SANTIKA.com</a>.</strong> Raditya Dewi Burhan Tapta Bafian
+    <strong>Copyright &copy; 2025 <a href="#">SANTIKA.com</a>.</strong>
 </footer>
--- resources/views/dosen/sidebar.blade.php
+++ resources/views/dosen/sidebar.blade.php
@@ -1,15 +1,11 @@
-<!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
         <img src={{ url('/adminlte/dist/img/AdminLTELogo.png') }} class="brand-image img-circle elevation-3"
             style="opacity: .8">
         <span class="brand-text font-weight-light">S A N T I K A</span>
     </a>
 
-    <!-- Sidebar -->
     <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
                 @if (is_null(auth('dosen')->user()->foto_profil))
@@ -26,13 +22,9 @@
                 </a>
             </div>
         </div>
-
-        <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                 data-accordion="false">
-                <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
                 <li class="nav-item" id="dashboard-item">
                     <!-- route("dosen.dashboard") -->
                     <a href="#" class="nav-link">
@@ -130,7 +122,5 @@
                         </li>
                     </ul>
         </nav>
-        <!-- /.sidebar-menu -->
     </div>
-    <!-- /.sidebar -->
 </aside>
